d7dab4ba45438f2dccb048d99b40f6e38214d111
Amr Soliman <thisissoliman@protonmail.com>
2024-01-23 17:42:42 +0200

    a function to create a new openai client

---
+++ src/ai_services/mod.rs
@@ -0,0 +1,21 @@
+// region: Modules
+
+use async_openai::{config::OpenAIConfig, Client};
+
+use crate::Result;
+
+// endregion: Modules
+
+// region: Client
+const OPEN_AI_KEY: &str = "OPENAI_API_KEY";
+
+pub fn new_openai_client() -> Result<Client<OpenAIConfig>> {
+    if std::env::var(OPEN_AI_KEY).is_ok() {
+        Ok(Client::new())
+    } else {
+        println!("please provide a key ");
+        Err("no api key found".into())
+    }
+}
+
+// endregion: Client
